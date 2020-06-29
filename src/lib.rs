d6b56b4b8eb65de4f60a2ca076f271764e0d9b52
Hansie Odendaal <pluto@tari.com>
2020-06-29 16:25:21 +0200

    enable nightly-2020-06-10 with 0.2.3

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![cfg_attr(not(test), no_std)]
 #![cfg_attr(feature = "nightly", feature(asm, i128_type, specialization))]
 #![deny(missing_docs)]
+#![feature(llvm_asm)]
 
 //! Helpers for clearing sensitive data on the stack and heap.
 //!
